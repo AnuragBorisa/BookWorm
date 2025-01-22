e3e6aa5b13cd38a7fe383f5736a7b968031c037f
Anurag Borisa <anuragborisa@gmail.com>
2025-01-22 14:41:40 +0530

    Initial Commit

---
+++ BookWorm/BookWormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookWormApp.swift
+//  BookWorm
+//
+//  Created by Anurag on 22/01/25.
+//
+
+import SwiftUI
+
+@main
+struct BookWormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
